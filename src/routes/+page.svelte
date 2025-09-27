a467d56c52b3282e6572050fdcfc5bd8eafe7cae
Emily Rose <hello@fauxtrots.com>
2025-09-27 16:09:43 -0400

    Initial setup

---
+++ src/routes/+page.svelte
@@ -0,0 +1,41 @@
+<hgroup>
+	<h2>I'm participating in</h2>
+	<a href="https://weirdweboctober.website/">
+		<h1>Weird Web October 2025</h1>
+	</a>
+</hgroup>
+
+<h3>Themes</h3>
+<ol>
+	<li>Transparency</li>
+	<li>Maps</li>
+	<li>Scramble</li>
+	<li>Filters</li>
+	<li>Remix</li>
+	<li>Solids</li>
+	<li>Sub-optimal</li>
+	<li>Containers</li>
+	<li>Blink</li>
+	<li>Warning</li>
+	<li>Camera</li>
+	<li>Battle</li>
+	<li>Upside-down</li>
+	<li>Doubles</li>
+	<li>Unicode</li>
+	<li>Transition</li>
+	<li>Illumination</li>
+	<li>Trading</li>
+	<li>Bounce</li>
+	<li>Hidden</li>
+	<li>Language</li>
+	<li>Memory</li>
+	<li>Style</li>
+	<li>Counter</li>
+	<li>Empty</li>
+	<li>Texture</li>
+	<li>Spell</li>
+	<li>Tables</li>
+	<li>Surprise</li>
+	<li>Deprecated</li>
+	<li>Spooky</li>
+</ol>
